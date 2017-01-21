79bd647a0ed4fdaa7575d93a81db1c44d622a8ca
lowrck <lowrck@gmail.com>
2017-01-21 12:49:16 -0600

    Initial commit

---
+++ MovieProjectServer/routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json({ type: 'application/*+json'});
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+
+
+  res.render('form', { title: 'Movie Search' });
+});
+router.post('/', function(req, res, next) {
+
+  var moviename = req.body.moviename;
+  console.log(req.body);
+  var url = "http://www.omdbapi.com/?r=json&t=" + encodeURIComponent(moviename);
+  console.log(moviename);
+  console.log(url);
+  request(url, function(error, response, body) {
+
+    console.log(body);
+    var data = JSON.parse(body);
+    var scoredata = data.imdbRating * 10;
+    res.render('index', {score: scoredata + "%"});
+
+  })
+
+})
+
+module.exports = router;
